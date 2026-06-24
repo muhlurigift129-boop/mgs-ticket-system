5dd9b30f499616d1fb99284affb4ab44e64a5e39
muhlurigift129-boop <muhlurigift129@gmail.com>
2026-06-24 15:43:53 +0200

    git add . git commit -m "Update config system" git push origin main

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,38 +1,41 @@
-import { initializeApp }
-from "firebase/app"
+import { initializeApp } from "firebase/app"
 
-import {
-  getFirestore
-}
-from "firebase/firestore"
+import { getFirestore } from "firebase/firestore"
+
+import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
 
   apiKey:
-  "AIzaSyAtLqZlGNtI6Y6Cb-M_qjDkx9p5EFylAt8",
+    "AIzaSyAtLqZlGNtI6Y6Cb-M_qjDkx9p5EFylAt8",
 
   authDomain:
-  "mgs-ticket.firebaseapp.com",
+    "mgs-ticket.firebaseapp.com",
 
   projectId:
-  "mgs-ticket",
+    "mgs-ticket",
 
   storageBucket:
-  "mgs-ticket.firebasestorage.app",
+    "mgs-ticket.firebasestorage.app",
 
   messagingSenderId:
-  "502379201015",
+    "502379201015",
 
   appId:
-  "1:502379201015:web:088f997afa9bb0aa0c6ef2",
+    "1:502379201015:web:088f997afa9bb0aa0c6ef2",
 
   measurementId:
-  "G-X8HP8D242C"
+    "G-X8HP8D242C"
 
 }
 
 const app =
-initializeApp(firebaseConfig)
+  initializeApp(firebaseConfig)
 
 export const db =
-getFirestore(app)
+  getFirestore(app)
+
+export const auth =
+  getAuth(app)
+
+export default app
