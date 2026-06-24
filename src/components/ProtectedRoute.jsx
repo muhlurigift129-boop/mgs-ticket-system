7490edffd341b715bc4509d34d9a8fd5a948c899
muhlurigift129-boop <muhlurigift129@gmail.com>
2026-06-24 18:01:59 +0200

    Create ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom"
+import { auth } from "../firebase/config"
+
+export default function ProtectedRoute({ children }) {
+
+  const user = auth.currentUser
+
+  if (!user) {
+
+    return (
+      <Navigate
+        to="/login"
+        replace
+      />
+    )
+
+  }
+
+  return children
+}
