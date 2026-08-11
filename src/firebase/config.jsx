8869303af38bc444263de0e88f4e678161fddd24
muhlurigift129-boop <muhlurigift129@gmail.com>
2026-08-11 17:55:47 +0200

    Update config.jsx

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,5 +1,6 @@
 // ==========================================
-// MGS TICKET SYSTEM - FIREBASE CONFIG
+// MGS TICKET SYSTEM
+// FIREBASE CONFIGURATION
 // ==========================================
 
 import { initializeApp } from "firebase/app";
@@ -7,22 +8,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // ==========================================
-// FIREBASE CONFIGURATION
+// FIREBASE CONFIG
 // ==========================================
 
 const firebaseConfig = {
   apiKey: "AIzaSyAtLqZlGNtI6Y6Cb-M_qjDkx9p5EFylAt8",
-
   authDomain: "mgs-ticket.firebaseapp.com",
-
   projectId: "mgs-ticket",
-
   storageBucket: "mgs-ticket.firebasestorage.app",
-
   messagingSenderId: "502379201015",
-
   appId: "1:502379201015:web:088f997afa9bb0aa0c6ef2",
-
   measurementId: "G-X8HP8D242C",
 };
 
@@ -48,10 +43,6 @@ const auth = getAuth(app);
 // EXPORT
 // ==========================================
 
-export {
-  app,
-  db,
-  auth,
-};
+export { app, db, auth };
 
 export default app;
